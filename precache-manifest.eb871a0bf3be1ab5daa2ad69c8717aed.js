d6fc9e94764e50493d5bf38444237dd346f98d9b
PeterTing <a0917209079@gmail.com>
2019-06-10 02:04:47 +0800

    init

---
+++ precache-manifest.eb871a0bf3be1ab5daa2ad69c8717aed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8561bea43d2f26f1945f5a483699e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9312cc00fd48359e0252",
+    "url": "/static/css/main.26b676b3.chunk.css"
+  },
+  {
+    "revision": "3adf8b2834059ee12028",
+    "url": "/static/js/2.1062f5e1.chunk.js"
+  },
+  {
+    "revision": "9312cc00fd48359e0252",
+    "url": "/static/js/main.1b001302.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c46a24214b31326025e6458a5f38bd4d",
+    "url": "/static/media/exhib_card_1.c46a2421.png"
+  },
+  {
+    "revision": "5ccc8f1e717d12aaf0e3d2cf9b294fb2",
+    "url": "/static/media/icon_close_blue.5ccc8f1e.svg"
+  },
+  {
+    "revision": "82bdd5e0fa5d431abec5860792a1b1e3",
+    "url": "/static/media/icon_eye.82bdd5e0.svg"
+  },
+  {
+    "revision": "bd4b3b5ee9bc19fd45a49a5574b7c0b7",
+    "url": "/static/media/icon_fb.bd4b3b5e.svg"
+  },
+  {
+    "revision": "7d69931992aff86fbc7ac245f184f60f",
+    "url": "/static/media/icon_heart.7d699319.svg"
+  },
+  {
+    "revision": "3c2b429adee4d1071d9f4fe84ce47d67",
+    "url": "/static/media/icon_ig.3c2b429a.svg"
+  },
+  {
+    "revision": "e3b9ec3c6eb55f807f036b67e72c8983",
+    "url": "/static/media/icon_logo.e3b9ec3c.svg"
+  },
+  {
+    "revision": "04bb77e2e114ad5acdd44dd8c41c8dc9",
+    "url": "/static/media/icon_play.04bb77e2.svg"
+  },
+  {
+    "revision": "add5adcf0014cada05b940af22110185",
+    "url": "/static/media/image_1.add5adcf.jpg"
+  },
+  {
+    "revision": "f727742ffe345af3477aee145807bd47",
+    "url": "/static/media/image_2.f727742f.png"
+  },
+  {
+    "revision": "f905e15d13b317b314dd89df35c5a303",
+    "url": "/static/media/mejs-controls.f905e15d.svg"
+  },
+  {
+    "revision": "42fdd12473a53573f1806963e61835cf",
+    "url": "/static/media/team_pic.42fdd124.png"
+  }
+]);
